6fde17f6b1813c3c2cdefb5cc5e27aa5a0ff326d
valmtv <valerii.matviiv@gmail.com>
2026-06-02 22:23:00 +0200

    Extend CORS Policy

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 # browsers block cross-origin requests by default
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
